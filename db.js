1016c3eb2a8611b3e202f1bc08f8639b3978c41b
walterfirmo <walterfirmobr@gmail.com>
2022-06-21 14:56:20 -0400

    WebAPI

---
+++ db.js
@@ -0,0 +1,45 @@
+const {v4} = require("uuid");
+
+global.users = [];
+
+
+function findUsers(){
+    return global.users;
+
+}
+
+function findUser(id){
+    return global.users.find(item => item.id === id);
+
+}
+
+function insertUser(user){
+    user.id = v4();
+    global.users.push(user);
+
+}
+
+function updateUser(id, user){
+    return global.users.forEach((item, index, array) => {
+        if(item.id === id){
+            user.id = id;
+            array[index] = user;
+        }
+    })
+}
+
+function deleteUser(id){
+    return global.users.forEach((item, index, array) => {
+        if(item.id === id)
+            array.splice(index, 1);
+    })
+
+}
+
+module.exports = {
+    findUsers,
+    findUser,
+    insertUser,
+    updateUser,
+    deleteUser
+}
